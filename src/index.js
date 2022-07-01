39ec4187deddb4a82598d038a457d6f0817cc397
L3GO321 <googlik701@gmail.com>
2022-07-01 10:24:57 +0300

    fix appveour

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 export default function character(arr) {
-    return arr.sort((prev, next) => {
-        if (prev.health < next.health) {
-            return 1;
-        }
-        return -1;
-    });
+  return arr.sort((prev, next) => {
+    if (prev.health < next.health) {
+      return 1;
+    }
+    return -1;
+  });
 }
